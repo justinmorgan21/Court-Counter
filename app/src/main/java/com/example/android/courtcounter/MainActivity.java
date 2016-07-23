df3f5ece673d856f1c2c04be42f87c32f3320b23
Justin Morgan <jmorgan@overlake.org>
2016-07-22 20:33:42 -0700

    add text views and buttons for Team A implement methods to make buttons functional

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int teamAScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayAScore();
+    }
+
+    public void increaseAScore3(View view) {
+        teamAScore += 3;
+        displayAScore();
+    }
+
+    public void increaseAScore2(View view) {
+        teamAScore += 2;
+        displayAScore();
+    }
+
+    public void increaseAScore1(View view) {
+        teamAScore += 1;
+        displayAScore();
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayAScore() {
+        TextView aScoreDisplay = (TextView) findViewById(R.id.team_a_score);
+        aScoreDisplay.setText("" + teamAScore);
     }
 }
